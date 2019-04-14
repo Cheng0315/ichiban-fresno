dc0ff2b973f436e2855bd5e2ae624a179377948f
Cheng0315 <csalvue@gmail.com>
2019-04-14 15:33:01 -0700

    add admin and plates migration files, add data to seed file

---
+++ db/migrate/20190414222040_create_plates.rb
@@ -0,0 +1,13 @@
+class CreatePlates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plates do |t|
+      t.string :name
+      t.string :description
+      t.float :price
+      t.integer :category_id
+      t.belongs_to :admin
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(email: 'admin@admin.com', password: 'admin')
+
+Plate.create(name: 'California Roll', description: 'In: Crabmeat, avocado', price: 6.45, category_id: 1, admin_id: 1)
+Plate.create(name: 'Spicy Tuna', description: 'In: Spicy Tuna (raw)', price: 7.25, category_id: 2, admin_id: 1)
+Plate.create(name: 'Salmon Roll', description: 'In: Salmon (raw)', price: 7.25, category_id: 3, admin_id: 1)
+Plate.create(name: "Sunbird's Roll", description: 'In: Crabmeat, avocado (deep fried)', price: 8.25, category_id: 4, admin_id: 1)
