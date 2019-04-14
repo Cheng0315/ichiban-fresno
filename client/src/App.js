e6ca65255765444648fc7193cedd80f60afe0c44
Cheng0315 <csalvue@gmail.com>
2019-04-14 16:11:10 -0700

    add componentDidMount and constructor to App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      plates: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/plates')
+      .then(response => response.json())
+      .then(json => this.setState({plates: json}))
+      .catch(error => console.log(error))
+  }
+
+  renderPlates() {
+    return this.state.plates.map(plate => <div><div>{plate.name}</div><div>{plate.price}</div><div>{plate.description}</div></div>)
+  }
+
   render() {
     return (
       <div >
-        Hello
+        {this.renderPlates()}
       </div>
     );
   }
