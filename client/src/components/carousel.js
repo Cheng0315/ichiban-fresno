ac75e91d74f954dcc8324405fb73c83111aeb61a
Cheng0315 <csalvue@gmail.com>
2019-04-18 01:06:24 -0700

    add drop shadow to image in carousel

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -8,7 +8,7 @@ const CarouselComponent = () => {
     <Carousel>
       <Carousel.Item className='carouselHeight'>
         <img
-          className="d-block w-100 imgHeight"
+          className="d-block imgHeight"
           src="https://mms17-makemysushi.netdna-ssl.com/wp-content/uploads/2017/05/DSC04685-01.jpeg"
           alt="First slide"
         />
@@ -21,7 +21,7 @@ const CarouselComponent = () => {
 
       <Carousel.Item className='carouselHeight'>
         <img
-          className="d-block w-100 imgHeight"
+          className="d-block imgHeight"
           src="https://res.cloudinary.com/simpleview/image/upload/crm/frisco/Sushi0-380f05265056a36_380f06be-5056-a36a-073ed8db1c463d56.jpg"
           alt="Third slide"
         />
@@ -34,7 +34,7 @@ const CarouselComponent = () => {
 
       <Carousel.Item className='carouselHeight'>
         <img
-          className="d-block w-100 imgHeight"
+          className="d-block imgHeight"
           src="https://cdn2.mypcoskitchen.com/wp-content/uploads/2017/09/sushi-mpk-1024x683.jpg"
           alt="Third slide"
         />
