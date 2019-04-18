663eef16b196eda2963574af6ec257afcb09d4c6
Cheng0315 <csalvue@gmail.com>
2019-04-17 22:27:12 -0700

    add navbBar.css file, add style to navbar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import '../css/navBar.css';
 
 const NavBar = () => {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
+    <header className="header">
+      <Navbar bg="light" expand="lg" className='navbar'>
         <div className="container">
-        <Navbar.Brand href="/">Ichiban-Fresno</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand className='nav-brand' href="/"><h2>Ichiban</h2></Navbar.Brand>
+        <Navbar.Toggle className="custom-toggler" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/#menu">Menu</Nav.Link>
-            <Nav.Link href="/#about-us">About Us</Nav.Link>
-            <Nav.Link href="/#opening-hours">Opening Hours</Nav.Link>
-            <Nav.Link href="/#contact-us">Contact Us</Nav.Link>
+            <Nav.Link className='nav-link' href="/">Home</Nav.Link>
+            <Nav.Link className='nav-link' href="/#menu">Menu</Nav.Link>
+            <Nav.Link className='nav-link' href="/#about-us">About Us</Nav.Link>
+            <Nav.Link className='nav-link' href="/#opening-hours">Opening Hours</Nav.Link>
+            <Nav.Link className='nav-link' href="/#contact-us">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         </div>
       </Navbar>
-    </div>
+    </header>
   )
 }
 
