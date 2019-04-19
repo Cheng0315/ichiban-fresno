c5ad2af03b51ccdcd71bb4bc504b1908bfd5832e
Cheng0315 <csalvue@gmail.com>
2019-04-19 01:26:02 -0700

    add background color and padding to opening hours boxes

--- client/src/components/openingHours.js
+++ client/src/components/openingHours.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import '../css/openingHours.css';
 
 const OpeningHours = () => {
   return (
@@ -8,20 +8,26 @@ const OpeningHours = () => {
       <h2 className='text-center'>Opening Hours</h2>
       <Row>
         <div className='text-center col-md-4 col-sm-12'>
-          <h5>MONDAY-FRIDAY</h5>
-          <div>10:30 AM - 8:30 PM</div>
-          <div>(Kitchen closes at 8:00 PM)</div>
+          <div className='opening-hrs-boxes'>
+            <h5>MONDAY-FRIDAY</h5>
+            <div>10:30 AM - 8:30 PM</div>
+            <div>(Kitchen closes at 8:00 PM)</div>
+          </div>
         </div>
 
         <div className='text-center col-md-4 col-sm-12'>
-          <h5>SATURDAY</h5>
-          <div>12:00 PM - 8:30 PM</div>
-          <div>(Kitchen closes at 8:00 PM)</div>
+          <div className='opening-hrs-boxes'>
+            <h5>SATURDAY</h5>
+            <div>12:00 PM - 8:30 PM</div>
+            <div>(Kitchen closes at 8:00 PM)</div>
+          </div>
         </div>
 
         <div className='text-center col-md-4 col-sm-12'>
-          <h5>SATURDAY</h5>
-          <div>CLOSED</div>
+          <div className='opening-hrs-boxes closed-day'>
+            <h5>SATURDAY</h5>
+            <div>CLOSED</div>
+          </div>
         </div>
       </Row>
     </div>
