44746cf48ba314bce4a8be12b579d0ee2db12a75
Cheng0315 <csalvue@gmail.com>
2019-04-19 16:26:45 -0700

    add aboutUs.css file, add style and css to about us component

--- client/src/components/aboutUs.js
+++ client/src/components/aboutUs.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import '../css/aboutUs.css';
 
 const AboutUs = () => {
   return (
     <div id="about-us">
       <h2 className="text-center">About Us</h2>
-      <p>Welcome to Ichiban Restaurant. Whether you’re celebrating, 
-        dining with business clients or 
-        want a night out with friends, 
-        we have created the perfect ambiance 
-        with a menu filled with your favorites 
-        japanese dishes. 
-      </p>
+      <p className="text-center">Welcome to Ichiban Restaurant. Whether you’re celebrating, dining with business clients,</p> 
+      <p className="text-center">or want a night out with friends, we have created the perfect ambiance with a menu</p>
+      <p className="text-center">filled with your favorites japanese dishes.</p>
     </div>
   )
 }
