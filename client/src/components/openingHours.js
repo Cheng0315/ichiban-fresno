b7a6fa1f03afab480ad9f90b176bf56d7fce84cf
Cheng0315 <csalvue@gmail.com>
2019-04-18 22:26:19 -0700

    add menu.css file, centered menu btns

--- client/src/components/openingHours.js
+++ client/src/components/openingHours.js
@@ -7,22 +7,22 @@ const OpeningHours = () => {
     <div id="opening-hours">
       <h2 className='text-center'>Opening Hours</h2>
       <Row>
-        <Col className='text-center'>
+        <div className='text-center'>
           <h5>MONDAY-FRIDAY</h5>
           <div>10:30 AM - 8:30 PM</div>
           <div>(Kitchen closes at 8:00 PM)</div>
-        </Col>
+        </div>
 
-        <Col className='text-center'>
+        <div className='text-center'>
           <h5>SATURDAY</h5>
           <div>12:00 PM - 8:30 PM</div>
           <div>(Kitchen closes at 8:00 PM)</div>
-        </Col>
+        </div>
 
-        <Col className='text-center'>
+        <div className='text-center'>
           <h5>SATURDAY</h5>
           <div>CLOSED</div>
-        </Col>
+        </div>
       </Row>
     </div>
   )
