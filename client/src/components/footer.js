7335a3a5b92a236102a2a031c3c1b28d9845967f
Cheng0315 <csalvue@gmail.com>
2019-04-20 00:38:01 -0700

    add footer.css file and style footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../css/footer.css';
 
 const Footer = () => {
   return (
     <div id="footer">
-      <h6 className="text-center">Copyright &copy; 2019 Ichiban, Inc. All Rights Reserved</h6>
-      <p className="text-center">Developed by Cheng Vue</p>
+      <div className='footer-content'>
+        <div className="text-center">Copyright &copy; 2019 Ichiban, Inc. All Rights Reserved - Developed by Cheng Vue</div>
+      </div>
     </div>
   )
 }
