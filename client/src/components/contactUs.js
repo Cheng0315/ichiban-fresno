490898676737b7953a2183477c8852272b63cb3a
Cheng0315 <csalvue@gmail.com>
2019-04-19 23:49:59 -0700

    style contactUs component, add h4 tag to contactUs component

--- client/src/components/contactUs.js
+++ client/src/components/contactUs.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import '../css/contactUs.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -8,42 +6,36 @@ const ContactUs = () => {
   return (
     <div id="contact-us">
       <h2 className="text-center">Contact Us</h2>
-      <Row>
-        <Col>
-          <div className='restaurant-info'>
+      <div className='row restaurant-info'>
+        <div className='col-lg-6 col-md-12 col-sm-12'>
+          <h4 className="text-center">Restaurant's Information</h4>
+          <div className='restaurant-info-style'>
             <div className='restaurant-phone'>
-              <span><FontAwesomeIcon icon="phone"/></span>
-              <div className='d-inline-block'>
-                <div><strong>Phone</strong></div>
-                <div>(559) 255-7615</div>
-              </div>
+              <p><span><FontAwesomeIcon icon="phone"/></span><strong> Phone</strong></p>
+              <p>(559) 255-7615</p>
             </div>
             <div className='restaurant-email'>
-              <span><FontAwesomeIcon icon="envelope"/></span>
-              <div className='d-inline-block'>
-                <div><strong>Email</strong></div>
-                <div>ichibanfresno@gmail.com</div>
-              </div>
+              <p><span><FontAwesomeIcon icon="envelope"/></span><strong> Email</strong></p>
+              <p>ichibanfresno@gmail.com</p>
             </div>
             <div className='restaurant-address'>
-              <span><FontAwesomeIcon icon="home"/></span>
-              <div className='d-inline-block'>
-                <div><strong>Address</strong></div>
-                <div>4863 E Butler Ave,</div>
-                <div>Fresno, CA 93727</div>
-              </div>
+              <p><span><FontAwesomeIcon icon="home"/></span><strong> Address</strong></p>
+              <p>4863 E Butler Ave,</p>
+              <p>Fresno, CA 93727</p>
+            </div>
+            <div className='restaurant-sc-md'>
+
             </div>
           </div>
-        </Col>
+        </div>
 
-        <Col>
-          <div className='restaurant-map'>
-            <iframe width="100%" height="450"
-              src={"https://www.google.com/maps/embed/v1/place?key=" + process.env.REACT_APP_GOOGLE_MAP_API_KEY + "&q=Ichiban,Fresno+CA"}>
-            </iframe>
-          </div>
-        </Col>
-      </Row>
+        <div className='col-lg-6 col-md-12 col-sm-12'>
+          <h4 className="text-center">Map</h4>
+          <iframe width="100%" height="330"
+            src={"https://www.google.com/maps/embed/v1/place?key=" + process.env.REACT_APP_GOOGLE_MAP_API_KEY + "&q=Ichiban,Fresno+CA"}>
+          </iframe>
+        </div>
+      </div>
     </div>
   )
 }
