d0855147f9e8933075da46986e2bfcb99c829436
Cheng0315 <csalvue@gmail.com>
2019-04-19 17:12:37 -0700

    add contactUs.css file, add style and css to contact us component

--- client/src/components/contactUs.js
+++ client/src/components/contactUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../css/contactUs.css';
 
 const ContactUs = () => {
   return (
@@ -8,23 +9,25 @@ const ContactUs = () => {
       <h2 className="text-center">Contact Us</h2>
       <Row>
         <Col>
-          <div>
-            <div><strong>Address</strong></div>
-            <div>4863 E Butler Ave,</div>
-            <div>Fresno, CA 93727</div>
-          </div>
-          <div>
-            <div><strong>Phone</strong></div>
-            <div>(559) 255-7615</div>
-          </div>
-          <div>
-            <div><strong>Email</strong></div>
-            <div>ichibanfresno@gmail.com</div>
+          <div className='restaurant-info'>
+            <div className='restaurant-phone'>
+              <div><strong>Phone</strong></div>
+              <div>(559) 255-7615</div>
+            </div>
+            <div className='restaurant-email'>
+              <div><strong>Email</strong></div>
+              <div>ichibanfresno@gmail.com</div>
+            </div>
+            <div className='restaurant-address'>
+              <div><strong>Address</strong></div>
+              <div>4863 E Butler Ave,</div>
+              <div>Fresno, CA 93727</div>
+            </div>
           </div>
         </Col>
 
         <Col>
-          <div>
+          <div className='restaurant-map'>
             <iframe width="100%" height="450"
               src={"https://www.google.com/maps/embed/v1/place?key=" + process.env.REACT_APP_GOOGLE_MAP_API_KEY + "&q=Ichiban,Fresno+CA"}>
             </iframe>
