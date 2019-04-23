4bb7d109f3723a3bcfe3f03f9ff7aca5d3b12f3d
Cheng0315 <csalvue@gmail.com>
2019-04-23 00:29:06 -0700

    add invalidate token and calidate login method to admin controller

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,18 @@
 class Admin < ApplicationRecord
   has_many :plates
+  validates_uniquesness_of :email
   has_secure_password
   has_secure_token :auth_token
+
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(email, password)
+    admin = Admin.find_by(email: email)
+
+    if admin && admin.authenticate(password)
+      admin
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/api/plates' => 'plates#index'
   post '/api/sign_in' => 'admin#sign_in'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :admin
 end
