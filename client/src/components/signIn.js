3f4a44ea2d97d83c86f52a9836b39c25658c2885
Cheng0315 <csalvue@gmail.com>
2019-04-22 21:00:43 -0700

    add signin.css file, add style and css to signin component

--- client/src/components/signIn.js
+++ client/src/components/signIn.js
@@ -1,31 +1,30 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import '../css/signIn.css';
 
 class SignIn extends Component {
 
   render(){
     return ( 
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <div className='container sign-in'>
+        <div className='sign-in-form'>
+          <h1>ICHIBAN</h1>
+          <h4>Sign in to Ichiban</h4>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicChecbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" className='btn-block' type="submit">
+              Sign In
+            </Button>
+          </Form>
+        </div>
+      </div>
     )
   }
 }
