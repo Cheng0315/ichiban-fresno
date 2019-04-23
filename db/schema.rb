89e3773632b5bf9481e82deb593409a397614fe4
Cheng0315 <csalvue@gmail.com>
2019-04-23 00:07:23 -0700

    add api controller and session controller, add admin auth_token column to admins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_090807) do
+ActiveRecord::Schema.define(version: 2019_04_23_070352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_04_20_090807) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
   end
 
   create_table "plates", force: :cascade do |t|
