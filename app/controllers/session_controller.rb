5129affc13110b1716325685c47e68200a430b80
Cheng0315 <csalvue@gmail.com>
2019-04-23 00:20:08 -0700

    ad create method, destroy method, and logout method to sessions controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,31 @@
-class SessionController < ApplicationController
+class SessionController < ApiController
+  skip_before_action :require_login, only: [:create], raise: false
+
+  def create
+    if admin = Admin.validate_login(params[:email, params[:password]])
+      allow_token_to_be_used_only_once_for(admin)
+      send_token_for_valid_login_of(admin)
+    else
+      render_unauthorized('Error with login')
+    end
+  end
+
+  def destroy
+    logout
+    head :ok
+  end
+
+  private
+
+  def send_token_for_valid_login_of(admin)
+    render json: {token: admin.auth_token}
+  end
+
+  def allow_token_to_be_used_only_once_for(admin)
+    admin.regenerate_auth_token
+  end
+
+  def logout
+    current_user.invalidate_token
+  end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ApplicationRecord
   has_many :plates
   has_secure_password
+  has_secure_token :auth_token
 end
