e07115d31d7b5d6942e59706a1dc3ac541a037a8
Cheng0315 <csalvue@gmail.com>
2019-04-22 23:45:57 -0700

    add onSubmit to signin component to fetch post request

--- client/src/components/signIn.js
+++ client/src/components/signIn.js
@@ -5,19 +5,37 @@ import '../css/signIn.css';
 
 class SignIn extends Component {
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (e.target.checkValidity()) {
+      const inputs = {...{}, email: e.target.elements.email.value, password: e.target.elements.password.value}
+      fetch('/api/sign_in', {
+        method: 'post',
+        body: JSON.stringify(inputs),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      .then(response => response.json())
+      .then(response => console.log(response))
+    } else {
+      console.log('oh no')
+    }
+  }
+
   render(){
     return ( 
       <div className='container sign-in'>
         <div className='sign-in-form'>
           <h1>ICHIBAN</h1>
           <h4>Sign in to Ichiban</h4>
-          <Form>
+          <Form onSubmit={event => this.handleSubmit(event)} noValidate>
             <Form.Group controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="email" placeholder="Email" name='email' ref='email' required/>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" name='password' ref='password' required/>
             </Form.Group>
             <Button variant="primary" className='btn-block' type="submit">
               Sign In
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/api/plates' => 'plates#index'
+  post '/api/sign_in' => 'admin#sign_in'
 end
