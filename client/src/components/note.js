1c40de95311a473e2a25f4d1cb748f3522d48e74
Cheng0315 <csalvue@gmail.com>
2019-05-12 03:24:55 -0700

    set up application's backend to use google cloud for production

--- client/src/components/note.js
+++ client/src/components/note.js
@@ -9,7 +9,10 @@ class Note extends React.Component {
     if (this.props.auth) {
       note = <div className='container'>
         <div className='alert alert-warning text-center' role="alert">
-          NOTE: The 'Update My Info' link is disabled because this is the only account for this application.
+          NOTE: The 'Update My Info' link is disabled to prevent the 
+          change of the email or password of this account, as this is 
+          the only account in this application. If the email or 
+          password changed, other users will not be able to log in.
         </div>
       </div>
     }
